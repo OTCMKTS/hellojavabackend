7464181f78f11d3264e1dcb61f81825f50baa229
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2021-08-28 14:10:24 +0000

    26

--- lib/datadog/appsec/contrib/rack/request_middleware.rb
+++ lib/datadog/appsec/contrib/rack/request_middleware.rb
@@ -3,3 +3,127 @@ require 'json'
 require_relative 'gateway/request'
 require_relative 'gateway/response'
 require_relative '../../ext'
+require_relative '../../instrumentation/gateway'
+require_relative '../../processor'
+require_relative '../../response'
+
+require_relative '../../../tracing/client_ip'
+require_relative '../../../tracing/contrib/rack/header_collection'
+
+module Datadog
+  module AppSec
+    module Contrib
+      module Rack
+        # Topmost Rack middleware for AppSec
+        # This should be inserted just below Datadog::Tracing::Contrib::Rack::TraceMiddleware
+        class RequestMiddleware
+          def initialize(app, opt = {})
+            @app = app
+
+            @oneshot_tags_sent = false
+          end
+
+          def call(env)
+            return @app.call(env) unless Datadog::AppSec.enabled?
+
+            processor = Datadog::AppSec.processor
+
+            return @app.call(env) if processor.nil? || !processor.ready?
+
+            # TODO: handle exceptions, except for @app.call
+
+            context = processor.activate_context
+            env['datadog.waf.context'] = context
+
+            gateway_request = Gateway::Request.new(env)
+
+            add_appsec_tags(processor, active_trace, active_span, env)
+
+            request_return, request_response = catch(::Datadog::AppSec::Ext::INTERRUPT) do
+              Instrumentation.gateway.push('rack.request', gateway_request) do
+                @app.call(env)
+              end
+            end
+
+            if request_response && request_response.any? { |action, _event| action == :block }
+              request_return = AppSec::Response.negotiate(env).to_rack
+            end
+
+            gateway_response = Gateway::Response.new(
+              request_return[2],
+              request_return[0],
+              request_return[1],
+              active_context: context
+            )
+
+            _response_return, response_response = Instrumentation.gateway.push('rack.response', gateway_response)
+
+            context.events.each do |e|
+              e[:response] ||= gateway_response
+              e[:request]  ||= gateway_request
+            end
+
+            AppSec::Event.record(*context.events)
+
+            if response_response && response_response.any? { |action, _event| action == :block }
+              request_return = AppSec::Response.negotiate(env).to_rack
+            end
+
+            request_return
+          ensure
+            if context
+              add_waf_runtime_tags(active_trace, context)
+              processor.deactivate_context
+            end
+          end
+
+          private
+
+          def active_trace
+            # TODO: factor out tracing availability detection
+
+            return unless defined?(Datadog::Tracing)
+
+            Datadog::Tracing.active_trace
+          end
+
+          def active_span
+            # TODO: factor out tracing availability detection
+
+            return unless defined?(Datadog::Tracing)
+
+            Datadog::Tracing.active_span
+          end
+
+          def add_appsec_tags(processor, trace, span, env)
+            return unless trace
+
+            trace.set_tag('_dd.appsec.enabled', 1)
+            trace.set_tag('_dd.runtime_family', 'ruby')
+            trace.set_tag('_dd.appsec.waf.version', Datadog::AppSec::WAF::VERSION::BASE_STRING)
+
+            if span && span.get_tag(Tracing::Metadata::Ext::HTTP::TAG_CLIENT_IP).nil?
+              request_header_collection = Datadog::Tracing::Contrib::Rack::Header::RequestHeaderCollection.new(env)
+
+              # always collect client ip, as this is part of AppSec provided functionality
+              Datadog::Tracing::ClientIp.set_client_ip_tag!(
+                span,
+                headers: request_header_collection,
+                remote_ip: env['REMOTE_ADDR']
+              )
+            end
+
+            if processor.ruleset_info
+              trace.set_tag('_dd.appsec.event_rules.version', processor.ruleset_info[:version])
+
+              unless @oneshot_tags_sent
+                # Small race condition, but it's inoccuous: worst case the tags
+                # are sent a couple of times more than expected
+                @oneshot_tags_sent = true
+
+                trace.set_tag('_dd.appsec.event_rules.loaded', processor.ruleset_info[:loaded].to_f)
+                trace.set_tag('_dd.appsec.event_rules.error_count', processor.ruleset_info[:failed].to_f)
+                trace.set_tag('_dd.appsec.event_rules.errors', JSON.dump(processor.ruleset_info[:errors]))
+                trace.set_tag('_dd.appsec.event_rules.addresses', JSON.dump(processor.addresses))
+
+                # En
