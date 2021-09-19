19b6f88186e0466e1e2c9cf81ad14bc4426f0320
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2021-09-19 20:46:43 +0000

    Versão inicial

---
+++ lib/datadog/core/environment/ext.rb
@@ -0,0 +1,8 @@
+require_relative '../../../ddtrace/version'
+
+module Datadog
+  module Core
+    module Environment
+      # @public_api Changing resource names, tag names, or environment variables creates breaking changes.
+      module Ext
+        #
