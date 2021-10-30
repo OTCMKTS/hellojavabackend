f99e01e35bd1b46541b339fd3eec2d57497e8b8f
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2021-10-30 21:45:04 +0000

    Update zeus.txt

--- lib/datadog/profiling/collectors/old_stack.rb
+++ lib/datadog/profiling/collectors/old_stack.rb
@@ -73,4 +73,121 @@ module Datadog
           # Cache this buffer, since it's pretty expensive to keep accessing it
           @stack_sample_event_recorder = recorder[Events::StackSample]
           # See below for details on why this is needed
-          @needs_process_waiter_workarou
+          @needs_process_waiter_workaround = Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.7')
+        end
+
+        def start
+          reset_cpu_time_tracking
+          perform
+        end
+
+        def perform
+          collect_and_wait
+        end
+
+        def collect_and_wait
+          run_time = Core::Utils::Time.measure do
+            collect_events
+          end
+
+          # Update wait time to throttle profiling
+          self.loop_wait_time = compute_wait_time(run_time)
+        end
+
+        def collect_events
+          events = []
+          current_wall_time_ns = get_current_wall_time_timestamp_ns
+
+          # Collect backtraces from each thread
+          threads_to_sample.each do |thread|
+            next unless thread.alive?
+            next if ignore_thread.is_a?(Proc) && ignore_thread.call(thread)
+
+            event = collect_thread_event(thread, current_wall_time_ns)
+            events << event unless event.nil?
+          end
+
+          # Send events to recorder
+          recorder.push(events) unless events.empty?
+
+          events
+        end
+
+        def collect_thread_event(thread, current_wall_time_ns)
+          locations = thread.backtrace_locations
+          return if locations.nil?
+
+          # Having empty locations means that the thread is alive, but we don't know what it's doing:
+          #
+          # 1. It can be starting up
+          #    ```
+          #    > Thread.new { sleep }.backtrace
+          #    => [] # <-- note the thread hasn't actually started running sleep yet, we got there first
+          #    ```
+          # 2. It can be running native code
+          #    ```
+          #    > t = Process.detach(fork { sleep })
+          #    => #<Process::Waiter:0x00007ffe7285f7a0 run>
+          #    > t.backtrace
+          #    => [] # <-- this can happen even minutes later, e.g. it's not a race as in 1.
+          #    ```
+          #    This effect has been observed in threads created by the Iodine web server and the ffi gem
+          #
+          # To give customers visibility into these threads, we replace the empty stack with one containing a
+          # synthetic placeholder frame, so that these threads are properly represented in the UX.
+          locations = SYNTHETIC_STACK_IN_NATIVE_CODE if locations.empty?
+
+          # Get actual stack size then trim the stack
+          stack_size = locations.length
+          locations = locations[0..(max_frames - 1)]
+
+          # Convert backtrace locations into structs
+          locations = convert_backtrace_locations(locations)
+
+          thread_id = thread.object_id
+          root_span_id, span_id, trace_resource = trace_identifiers_helper.trace_identifiers_for(thread)
+          cpu_time = get_cpu_time_interval!(thread)
+          wall_time_interval_ns =
+            get_elapsed_since_last_sample_and_set_value(thread, THREAD_LAST_WALL_CLOCK_KEY, current_wall_time_ns)
+
+          Events::StackSample.new(
+            nil,
+            locations,
+            stack_size,
+            thread_id,
+            root_span_id,
+            span_id,
+            trace_resource,
+            cpu_time,
+            wall_time_interval_ns
+          )
+        end
+
+        def get_cpu_time_interval!(thread)
+          return unless cpu_time_provider
+
+          current_cpu_time_ns = cpu_time_provider.cpu_time_ns_for(thread)
+
+          return unless current_cpu_time_ns
+
+          get_elapsed_since_last_sample_and_set_value(thread, THREAD_LAST_CPU_TIME_KEY, current_cpu_time_ns)
+        end
+
+        def compute_wait_time(used_time)
+          # We took used_time to get the last sample.
+          #
+          # What we're computing here is -- if used_time corresponds to max_time_usage_pct of the time we should
+          # spend working, how much is (100% - max_time_usage_pct) of the time?
+          #
+          # For instance, if we took 10ms to sample, and max_time_usage_pct is 1%, then the other 99% is 990ms, which
+          # means we need to sleep for 990ms to guarantee that we don't spend more than 1% of the time working.
+          used_time_ns = used_time * 1e9
+          interval = (used_time_ns / (max_time_usage_pct / 100.0)) - used_time_ns
+          [interval / 1e9, MIN_INTERVAL].max
+        end
+
+        # Convert backtrace locations into structs
+        # Re-use old backtrace location objects if they already exist in the buffer
+        def convert_backtrace_locations(locations)
+          locations.collect do |location|
+            # Re-use existing BacktraceLocat
