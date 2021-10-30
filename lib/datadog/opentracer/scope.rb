20724d4f27dab75c87a93313fcd3759a7d080c41
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2021-10-30 07:22:09 +0000

    combo

---
+++ lib/datadog/opentracer/scope.rb
@@ -0,0 +1,16 @@
+module Datadog
+  module OpenTracer
+    # OpenTracing adapter for scope
+    # @public_api
+    class Scope < ::OpenTracing::Scope
+      attr_reader \
+        :manager,
+        :span
+
+      def initialize(manager:, span:)
+        @manager = manager
+        @span = span
+      end
+    end
+  end
+end
---
+++ lib/datadog/opentracer/scope_manager.rb
@@ -0,0 +1,7 @@
+module Datadog
+  module OpenTracer
+    # @public_api
+    class ScopeManager < ::OpenTracing::ScopeManager
+    end
+  end
+end
