55f1a080b84a4de181c306e55e052705b2d212a1
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2021-11-06 22:51:20 +0000

    maj doc

---
+++ lib/datadog/profiling/trace_identifiers/helper.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'ddtrace'
+
+module Datadog
+  module Profiling
+    module TraceIdentifiers
+      # Helper used to retrieve the trace identifiers (root span id and span id) for a given thread,
+      # if there is an active trace for that thread for the supported tracing APIs.
+      #
+      # This data is used to connect profiles to the traces -- samples in a profile will be tagged with this data and
+      # the profile can be filtered down to look at only the samples for a given trace.
+      class Helper
+        DEFAULT_SUPPORTED_APIS = [
+          Profiling::TraceIdentifiers::Ddtrace
+        ].freeze
+        private_constant :DEFAULT_SUPPORTED_APIS
+
+        def initialize(
+          tracer:,
+          # If this is disabled, the helper will strip the optional trace_resource_container even if provided by the api
+          endpoint_collection_enabled:,
+          supported_apis: DEFAULT_SUPPORTED_APIS.map { |api| api.new(tracer: tracer) }
+        )
+          @endpoint_collection_enabled = endpoint_collection_enabled
+  
