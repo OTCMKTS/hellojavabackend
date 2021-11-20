776063de993a6c5bf8a2f3393a861b3e7052f79e
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2021-11-20 05:50:19 +0000

    remove bin

---
+++ lib/datadog/tracing/contrib/action_mailer/configuration/settings.rb
@@ -0,0 +1,9 @@
+require_relative '../../configuration/settings'
+require_relative '../ext'
+
+module Datadog
+  module Tracing
+    module Contrib
+      module ActionMailer
+        module Configuration
+          # Cu
