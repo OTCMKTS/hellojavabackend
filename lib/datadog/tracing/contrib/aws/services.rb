32a6de08553926118ae65833d8118591eb86ad02
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2021-12-04 05:35:57 +0000

    net/wireless: ipw2200: Fix panic occurring in ipw_handle_promiscuous_tx()

---
+++ lib/datadog/tracing/contrib/aws/services.rb
@@ -0,0 +1,16 @@
+
+module Datadog
+  module Tracing
+    module Contrib
+      module Aws
+        SERVICES = %w[
+          ACM
+          APIGateway
+          AppStream
+          ApplicationAutoScaling
+          ApplicationDiscoveryService
+          Athena
+          AutoScaling
+          Batch
+          Budgets
+          CloudDirectory
