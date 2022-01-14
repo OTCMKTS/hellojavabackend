aa95b53c221036d8fac32d8501e3e2040d055c13
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-01-14 18:28:00 +0000

    updating portuguese and italian translations

--- lib/datadog/tracing/contrib/kafka/events/producer/deliver_messages.rb
+++ lib/datadog/tracing/contrib/kafka/events/producer/deliver_messages.rb
@@ -1,3 +1,4 @@
+
 require_relative '../../ext'
 require_relative '../../event'
 
@@ -6,29 +7,32 @@ module Datadog
     module Contrib
       module Kafka
         module Events
-          module ProduceOperation
-            # Defines instrumentation for send_messages.producer.kafka event
-            module SendMessages
+          module Producer
+            # Defines instrumentation for deliver_messages.producer.kafka event
+            module DeliverMessages
               include Kafka::Event
 
-              EVENT_NAME = 'send_messages.producer.kafka'.freeze
+              EVENT_NAME = 'deliver_messages.producer.kafka'.freeze
 
               def self.process(span, _event, _id, payload)
                 super
 
+                span.set_tag(Ext::TAG_ATTEMPTS, payload[:attempts]) if payload.key?(:attempts)
                 span.set_tag(Ext::TAG_MESSAGE_COUNT, payload[:message_count]) if payload.key?(:message_count)
-                span.set_tag(Ext::TAG_SENT_MESSAGE_COUNT, payload[:sent_message_count]) if payload.key?(:sent_message_count)
+                if payload.key?(:delivered_message_count)
+                  span.set_tag(Ext::TAG_DELIVERED_MESSAGE_COUNT, payload[:delivered_message_count])
+                end
                 span.set_tag(Tracing::Metadata::Ext::TAG_KIND, Tracing::Metadata::Ext::SpanKind::TAG_PRODUCER)
               end
 
               module_function
 
               def span_name
-                Ext::SPAN_SEND_MESSAGES
+                Ext::SPAN_DELIVER_MESSAGES
               end
 
               def span_options
-                super.merge({ tags: { Tracing::Metadata::Ext::TAG_OPERATION => Ext::TAG_OPERATION_SEND_MESSAGES } })
+                super.merge({ tags: { Tracing::Metadata::Ext::TAG_OPERATION => Ext::TAG_OPERATION_DELIVER_MESSAGES } })
               end
             end
           end
