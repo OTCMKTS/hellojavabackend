f568c18d088ff2d057fe4c2d3855a32e4e0fadfa
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-02-13 19:16:57 +0000

    deploy: vitruv-tools/Vitruv-Domains-AutomotiveSoftwareSystems@2b184ab1cb4809af1a4c49f8a67accd8741c2405

--- lib/datadog/tracing/contrib/redis/vendor/resolver.rb
+++ lib/datadog/tracing/contrib/redis/vendor/resolver.rb
@@ -27,4 +27,113 @@ module Datadog
               password: nil,
               db: 0 # ,
               # :driver => nil,
- 
+              # :id => nil,
+              # :tcp_keepalive => 0,
+              # :reconnect_attempts => 1,
+              # :reconnect_delay => 0,
+              # :reconnect_delay_max => 0.5,
+              # :inherit_socket => false
+            }.freeze
+
+            def resolve(options)
+              _parse_options(options)
+            end
+
+            # rubocop:disable Metrics/AbcSize
+            # rubocop:disable Metrics/MethodLength
+            # rubocop:disable Metrics/PerceivedComplexity
+            #
+            # This method is a subset of the implementation provided in v3.0.0
+            # https://github.com/redis/redis-rb/blob/v3.0.0/lib/redis/client.rb
+            # https://github.com/redis/redis-rb/blob/v4.1.3/lib/redis/client.rb
+            #
+            # Since it has been backported from the original gem, some linting
+            # cops have been disabled
+            def _parse_options(options)
+              # https://github.com/redis/redis-rb/blob/v4.1.3/lib/redis/client.rb#L404
+              # Early return for modern client options
+              return options if options[:_parsed]
+
+              defaults = DEFAULTS.dup
+              options = options.dup
+
+              defaults.each_key do |key|
+                # Fill in defaults if needed
+                defaults[key] = defaults[key].call if defaults[key].respond_to?(:call)
+
+                # Symbolize only keys that are needed
+                options[key] = options[key.to_s] if options.key?(key.to_s)
+              end
+
+              url = options[:url]
+              url = defaults[:url] if url.nil?
+
+              # Override defaults from URL if given
+              if url
+                uri = URI(url)
+
+                case uri.scheme
+                when 'unix'
+                  defaults[:path] = uri.path
+                when 'redis', 'rediss'
+                  defaults[:scheme]   = uri.scheme
+                  defaults[:host]     = uri.host if uri.host
+                  defaults[:port]     = uri.port if uri.port
+                  defaults[:password] = CGI.unescape(uri.password) if uri.password
+                  defaults[:db]       = uri.path[1..-1].to_i if uri.path
+                  defaults[:role] = :master
+                else
+                  raise ArgumentError, "invalid uri scheme '#{uri.scheme}'"
+                end
+
+                # defaults[:ssl] = true if uri.scheme == "rediss"
+              end
+
+              # Use default when option is not specified or nil
+              defaults.each_key do |key|
+                options[key] = defaults[key] if options[key].nil?
+              end
+
+              if options[:path]
+                # Unix socket
+                options[:scheme] = 'unix'
+                options.delete(:host)
+                options.delete(:port)
+              else
+                # TCP socket
+                options[:host] = options[:host].to_s
+                options[:port] = options[:port].to_i
+              end
+
+              # Options ignored by the integration
+              #
+              # if options.has_key?(:timeout)
+              #   options[:connect_timeout] ||= options[:timeout]
+              #   options[:read_timeout]    ||= options[:timeout]
+              #   options[:write_timeout]   ||= options[:timeout]
+              # end
+
+              # options[:connect_timeout] = Float(options[:connect_timeout])
+              # options[:read_timeout]    = Float(options[:read_timeout])
+              # options[:write_timeout]   = Float(options[:write_timeout])
+
+              # options[:reconnect_attempts] = options[:reconnect_attempts].to_i
+              # options[:reconnect_delay] = options[:reconnect_delay].to_f
+              # options[:reconnect_delay_max] = options[:reconnect_delay_max].to_f
+
+              options[:db] = options[:db].to_i
+              # options[:driver] = _parse_driver(options[:driver]) || Connection.drivers.last
+
+              # case options[:tcp_keepalive]
+              # when Hash
+              #   [:time, :intvl, :probes].each do |key|
+              #     unless options[:tcp_keepalive][key].is_a?(Integer)
+              #       raise "Expected the #{key.inspect} key in :tcp_keepalive to be an Integer"
+              #     end
+              #   end
+
+              # when Integer
+              #   if options[:tcp_keepalive] >= 60
+              #     options[:tcp_keepalive] = {:time => options[:tcp_keepalive] - 20, :intvl => 10, :probes => 2}
+
+  
