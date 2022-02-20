d9fd2cabdfd0111abb359528208159540e426282
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-02-20 07:56:15 +0000

    Javadoc fixes

--- lib/datadog/tracing/contrib/rest_client/request_patch.rb
+++ lib/datadog/tracing/contrib/rest_client/request_patch.rb
@@ -45,4 +45,60 @@ module Datadog
               Contrib::Analytics.set_sample_rate(span, analytics_sample_rate) if analytics_enabled?
 
               span.set_tag(Tracing::Metadata::Ext::HTTP::TAG_URL, uri.path)
-              span.set_tag(Tracing::Metadata::Ext::H
+              span.set_tag(Tracing::Metadata::Ext::HTTP::TAG_METHOD, method.to_s.upcase)
+              span.set_tag(Tracing::Metadata::Ext::NET::TAG_TARGET_HOST, uri.host)
+              span.set_tag(Tracing::Metadata::Ext::NET::TAG_TARGET_PORT, uri.port)
+            end
+
+            def datadog_trace_request(uri)
+              span = Tracing.trace(
+                Ext::SPAN_REQUEST,
+                service: datadog_configuration[:split_by_domain] ? uri.host : datadog_configuration[:service_name],
+                span_type: Tracing::Metadata::Ext::HTTP::TYPE_OUTBOUND
+              )
+
+              trace = Tracing.active_trace
+
+              datadog_tag_request(uri, span)
+
+              yield(span, trace).tap do |response|
+                # Verify return value is a response
+                # If so, add additional tags.
+                if response.is_a?(::RestClient::Response)
+                  span.set_tag(Tracing::Metadata::Ext::HTTP::TAG_STATUS_CODE, response.code)
+                end
+              end
+            rescue ::RestClient::ExceptionWithResponse => e
+              span.set_error(e) if Tracing::Metadata::Ext::HTTP::ERROR_RANGE.cover?(e.http_code)
+              span.set_tag(Tracing::Metadata::Ext::HTTP::TAG_STATUS_CODE, e.http_code)
+
+              raise e
+              # rubocop:disable Lint/RescueException
+            rescue Exception => e
+              # rubocop:enable Lint/RescueException
+              span.set_error(e) if span
+
+              raise e
+            ensure
+              span.finish if span
+            end
+
+            private
+
+            def datadog_configuration
+              Datadog.configuration.tracing[:rest_client]
+            end
+
+            def analytics_enabled?
+              Contrib::Analytics.enabled?(datadog_configuration[:analytics_enabled])
+            end
+
+            def analytics_sample_rate
+              datadog_configuration[:analytics_sample_rate]
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/datadog/tracing/contrib/semantic_logger/instrumentation.rb
@@ -0,0 +1,41 @@
+require_relative '../../../core/logging/ext'
+
+module Datadog
+  module Tracing
+    module Contrib
+      module SemanticLogger
+        # Instrumentation for SemanticLogger
+        module Instrumentation
+          def self.included(base)
+            base.prepend(InstanceMethods)
+          end
+
+          # Instance methods for configuration
+          module InstanceMethods
+            def log(log, message = nil, progname = nil, &block)
+              return super unless Datadog.configuration.tracing[:semantic_logger].enabled
+
+              original_named_tags = log.named_tags || {}
+
+              # Retrieves trace information for current thread
+              correlation = Tracing.correlation
+              # merge original lambda with datadog context
+
+              datadog_trace_log_hash = {
+                # Adds IDs as tags to log output
+                dd: {
+                  # To preserve precision during JSON serialization, use strings for large numbers
+                  trace_id: correlation.trace_id.to_s,
+                  span_id: correlation.span_id.to_s,
+                  env: correlation.env.to_s,
+                  service: correlation.service.to_s,
+                  version: correlation.version.to_s
+                },
+                ddsource: Core::Logging::Ext::DD_SOURCE
+              }
+
+              # # if the user already has conflicting log_tags
+              # # we want them to clobber ours, because we should allow them to override
+              # # if needed.
+              log.named_tags = datadog_trace_log_hash.merge(original_named_tags)
+              super(log, 
