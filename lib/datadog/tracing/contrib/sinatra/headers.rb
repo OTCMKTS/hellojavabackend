0fe793d2c69102282de672164dd26a7fd82a92f8
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-03-04 12:08:19 +0000

    [maven-release-plugin] prepare release v0.2.1

--- lib/datadog/tracing/contrib/sinatra/headers.rb
+++ lib/datadog/tracing/contrib/sinatra/headers.rb
@@ -15,4 +15,19 @@ module Datadog
               target_headers.each do |header|
                 if headers.key?(header)
                   result[Tracing::Metadata::Ext::HTTP::ResponseHeaders.to_tag(header)] = headers[header]
-           
+                else
+                  # Try a case-insensitive lookup
+                  uppercased_header = header.to_s.upcase
+                  matching_header = headers.keys.find { |h| h.upcase == uppercased_header }
+                  if matching_header
+                    result[Tracing::Metadata::Ext::HTTP::ResponseHeaders.to_tag(header)] = headers[matching_header]
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
