b47f1820bda3555cb2751154832964ff2dd1efc1
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-03-31 09:01:05 +0000

    same as previous

--- lib-injection/auto_inject.rb
+++ lib-injection/auto_inject.rb
@@ -31,4 +31,47 @@ begin
       "bundle add ddtrace --require ddtrace/auto_instrument --version #{version.gsub(/^v/, '').shellescape}"
     elsif !sha.empty?
       # For internal testing
-      "bundle add ddtrace --require ddtrace/aut
+      "bundle add ddtrace --require ddtrace/auto_instrument --github datadog/dd-trace-rb --ref #{sha.shellescape}"
+    end
+
+  unless bundle_add_ddtrace_cmd
+    STDERR.puts "[ddtrace] #{failure_prefix} Missing version specification. #{support_message}"
+    return
+  end
+
+  STDOUT.puts "[ddtrace] Performing lib injection with `#{bundle_add_ddtrace_cmd}`" if ENV['DD_TRACE_DEBUG'] == 'true'
+
+  gemfile   = Bundler::SharedHelpers.default_gemfile
+  lockfile  = Bundler::SharedHelpers.default_lockfile
+
+  datadog_gemfile  = gemfile.dirname  + "datadog-Gemfile"
+  datadog_lockfile = lockfile.dirname + "datadog-Gemfile.lock"
+
+  require 'fileutils'
+
+  begin
+    # Copies for trial
+    FileUtils.cp gemfile, datadog_gemfile
+    FileUtils.cp lockfile, datadog_lockfile
+
+    output, status = Open3.capture2e(
+      { 'DD_TRACE_SKIP_LIB_INJECTION' => 'true', 'BUNDLE_GEMFILE' => datadog_gemfile.to_s },
+      bundle_add_ddtrace_cmd
+    )
+
+    if status.success?
+      STDOUT.puts '[ddtrace] Datadog lib injection successfully added ddtrace to the application.'
+
+      FileUtils.cp datadog_gemfile, gemfile
+      FileUtils.cp datadog_lockfile, lockfile
+    else
+      STDERR.puts "[ddtrace] #{failure_prefix} Unable to add ddtrace. Error output:\n#{output.split("\n").map {|l| "[ddtrace] #{l}"}.join("\n")}\n#{support_message}"
+    end
+  ensure
+    # Remove the copies
+    FileUtils.rm datadog_gemfile
+    FileUtils.rm datadog_lockfile
+  end
+rescue Exception => e
+  STDERR.puts "[ddtrace] #{failure_prefix} #{e.class.name} #{e.message}\nBacktrace: #{e.backtrace.join("\n")}\n#{support_message}"
+end
