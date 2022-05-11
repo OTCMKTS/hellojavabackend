8ebc5561de84949b422cd82d06591da71d100dd9
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-05-11 17:04:09 +0000

    minor modifications

--- spec/datadog/core/configuration/components_spec.rb
+++ spec/datadog/core/configuration/components_spec.rb
@@ -42,4 +42,149 @@ RSpec.describe Datadog::Core::Configuration::Components do
   before do
     # Ensure the real task never gets run (so it doesn't apply our thread patches and other extensions to our test env)
     if Datadog::Profiling.supported?
-      allow(Datadog::Profiling::Tasks::Setup).to receive(:new).and_retu
+      allow(Datadog::Profiling::Tasks::Setup).to receive(:new).and_return(profiler_setup_task)
+    end
+    allow(Datadog::Statsd).to receive(:new) { instance_double(Datadog::Statsd) }
+  end
+
+  describe '::new' do
+    let(:logger) { instance_double(Datadog::Core::Logger) }
+    let(:tracer) { instance_double(Datadog::Tracing::Tracer) }
+    let(:profiler) { Datadog::Profiling.supported? ? instance_double(Datadog::Profiling::Profiler) : nil }
+    let(:runtime_metrics) { instance_double(Datadog::Core::Workers::RuntimeMetrics) }
+    let(:health_metrics) { instance_double(Datadog::Core::Diagnostics::Health::Metrics) }
+
+    before do
+      expect(described_class).to receive(:build_logger)
+        .with(settings)
+        .and_return(logger)
+
+      expect(described_class).to receive(:build_tracer)
+        .with(settings, instance_of(Datadog::Core::Configuration::AgentSettingsResolver::AgentSettings))
+        .and_return(tracer)
+
+      expect(described_class).to receive(:build_profiler)
+        .with(
+          settings,
+          instance_of(Datadog::Core::Configuration::AgentSettingsResolver::AgentSettings),
+          tracer
+        )
+        .and_return(profiler)
+
+      expect(described_class).to receive(:build_runtime_metrics_worker)
+        .with(settings)
+        .and_return(runtime_metrics)
+
+      expect(described_class).to receive(:build_health_metrics)
+        .with(settings)
+        .and_return(health_metrics)
+    end
+
+    after do
+      components.telemetry.worker.stop(true)
+      components.telemetry.worker.join
+    end
+
+    it do
+      expect(components.logger).to be logger
+      expect(components.tracer).to be tracer
+      expect(components.profiler).to be profiler
+      expect(components.runtime_metrics).to be runtime_metrics
+      expect(components.health_metrics).to be health_metrics
+    end
+  end
+
+  describe '::build_health_metrics' do
+    subject(:build_health_metrics) { described_class.build_health_metrics(settings) }
+
+    context 'given settings' do
+      shared_examples_for 'new health metrics' do
+        let(:health_metrics) { instance_double(Datadog::Core::Diagnostics::Health::Metrics) }
+        let(:default_options) { { enabled: settings.diagnostics.health_metrics.enabled } }
+        let(:options) { {} }
+
+        before do
+          expect(Datadog::Core::Diagnostics::Health::Metrics).to receive(:new)
+            .with(default_options.merge(options))
+            .and_return(health_metrics)
+        end
+
+        it { is_expected.to be(health_metrics) }
+      end
+
+      context 'by default' do
+        it_behaves_like 'new health metrics'
+      end
+
+      context 'with :enabled' do
+        let(:enabled) { double('enabled') }
+
+        before do
+          allow(settings.diagnostics.health_metrics)
+            .to receive(:enabled)
+            .and_return(enabled)
+        end
+
+        it_behaves_like 'new health metrics' do
+          let(:options) { { enabled: enabled } }
+        end
+      end
+
+      context 'with :statsd' do
+        let(:statsd) { instance_double(Datadog::Statsd) }
+
+        before do
+          allow(settings.diagnostics.health_metrics)
+            .to receive(:statsd)
+            .and_return(statsd)
+        end
+
+        it_behaves_like 'new health metrics' do
+          let(:options) { { statsd: statsd } }
+        end
+      end
+    end
+  end
+
+  describe '::build_logger' do
+    subject(:build_logger) { described_class.build_logger(settings) }
+
+    context 'given an instance' do
+      let(:instance) { instance_double(Datadog::Core::Logger) }
+
+      before do
+        expect(settings.logger).to receive(:instance)
+          .and_return(instance)
+
+        expect(instance).to receive(:level=)
+          .with(settings.logger.level)
+      end
+
+      it 'uses the logger instance' do
+        expect(Datadog::Core::Logger).to_not receive(:new)
+        is_expected.to be(instance)
+      end
+    end
+
+    context 'given settings' do
+      shared_examples_for 'new logger' do
+        let(:logger) { instance_double(Datadog::Core::Logger) }
+        let(:level) { settings.logger.level }
+
+        before do
+          expect(Datadog::Core::Logger).to receive(:new)
+            .with($stdout)
+            .and_return(logger)
+
+          expect(logger).to receive(:level=).with(level)
+        end
+
+        it { is_expected.to be(logger) }
+      end
+
+      context 'by default' do
+        it_behaves_like 'new logger'
+      end
+
+      context 'with :level' do
+        let(:level) { double('level
