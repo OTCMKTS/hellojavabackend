243463c2112dff27dd973f181f514e7ba01a08d4
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-05-28 07:40:06 +0000

    remove unused test

--- spec/datadog/core/configuration_spec.rb
+++ spec/datadog/core/configuration_spec.rb
@@ -248,4 +248,144 @@ RSpec.describe Datadog::Core::Configuration do
         end
 
         context 'are not changed' do
-          let(:statsd) { instance_double(Datadog::S
+          let(:statsd) { instance_double(Datadog::Statsd) }
+
+          before do
+            expect(statsd).to_not receive(:close)
+
+            test_class.configure do |c|
+              c.runtime_metrics.statsd = statsd
+              c.diagnostics.health_metrics.statsd = statsd
+            end
+
+            test_class.configure { |_c| }
+          end
+
+          it 'reuses the same Statsd' do
+            expect(test_class.send(:components).runtime_metrics.metrics.statsd).to be statsd
+          end
+        end
+      end
+
+      context 'when the tracer' do
+        context 'is replaced' do
+          let(:old_tracer) { Datadog::Tracing::Tracer.new }
+          let(:new_tracer) { Datadog::Tracing::Tracer.new }
+
+          before do
+            expect(old_tracer).to receive(:shutdown!)
+
+            test_class.send(:configure) { |c| c.tracing.instance = old_tracer }
+            test_class.send(:configure) { |c| c.tracing.instance = new_tracer }
+          end
+
+          it 'replaces the old tracer and shuts it down' do
+            expect(test_class.send(:components).tracer).to be new_tracer
+          end
+        end
+
+        context 'is reused' do
+          let(:tracer) { Datadog::Tracing::Tracer.new }
+
+          before do
+            expect(tracer).to_not receive(:shutdown!)
+
+            test_class.send(:configure) { |c| c.tracing.instance = tracer }
+            test_class.send(:configure) { |c| c.tracing.instance = tracer }
+          end
+
+          it 'reuses the same tracer' do
+            expect(test_class.send(:components).tracer).to be tracer
+          end
+        end
+
+        context 'is not changed' do
+          let(:tracer) { Datadog::Tracing::Tracer.new }
+
+          before do
+            expect(tracer).to_not receive(:shutdown!)
+
+            test_class.send(:configure) { |c| c.tracing.instance = tracer }
+            test_class.send(:configure) { |_c| }
+          end
+
+          it 'reuses the same tracer' do
+            expect(test_class.send(:components).tracer).to be tracer
+          end
+        end
+      end
+
+      context 'when the profiler' do
+        context 'is not changed' do
+          before { skip_if_profiling_not_supported(self) }
+
+          context 'and profiling is enabled' do
+            before do
+              allow(test_class.configuration.profiling)
+                .to receive(:enabled)
+                .and_return(true)
+
+              allow_any_instance_of(Datadog::Profiling::Profiler)
+                .to receive(:start)
+              allow_any_instance_of(Datadog::Profiling::Tasks::Setup)
+                .to receive(:run)
+            end
+
+            it 'starts the profiler' do
+              configure
+              expect(test_class.send(:components).profiler).to have_received(:start)
+            end
+          end
+        end
+      end
+
+      context 'when reconfigured multiple times' do
+        context 'with runtime metrics active' do
+          before do
+            test_class.configure do |c|
+              c.runtime_metrics.enabled = true
+            end
+
+            @old_runtime_metrics = test_class.send(:components).runtime_metrics
+
+            test_class.configure do |c|
+              c.runtime_metrics.enabled = true
+            end
+          end
+
+          it 'stops the old runtime metrics worker' do
+            expect(@old_runtime_metrics.enabled?).to be false
+            expect(@old_runtime_metrics.running?).to be false
+
+            expect(test_class.send(:components).runtime_metrics).to_not be @old_runtime_metrics
+
+            expect(test_class.send(:components).runtime_metrics.enabled?).to be true
+            expect(test_class.send(:components).runtime_metrics.running?).to be false
+          end
+        end
+      end
+    end
+
+    describe '#configure_onto' do
+      subject(:configure_onto) { test_class.configure_onto(object, **options) }
+
+      let(:object) { Object.new }
+      let(:options) { { any: :thing } }
+
+      it 'attaches a pin to the object' do
+        expect(Datadog::Core::Pin)
+          .to receive(:set_on)
+          .with(object, **options)
+
+        configure_onto
+      end
+    end
+
+    describe '#configuration_for' do
+      subject(:configuration_for) { test_class.configuration_for(object, option_name) }
+
+      let(:object) { double('object') }
+      let(:option_name) { :a_setting }
+
+      context 'when the object has not been configured' do
+        it { is_expected.to be nil }
