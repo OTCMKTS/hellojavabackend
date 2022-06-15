76ce732b8b8b5d1715f9adca66e8958ea9bcab48
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-06-15 15:27:05 +0000

    Add User model

--- spec/datadog/profiling/collectors/code_provenance_spec.rb
+++ spec/datadog/profiling/collectors/code_provenance_spec.rb
@@ -54,4 +54,134 @@ RSpec.describe Datadog::Profiling::Collectors::CodeProvenance do
           instance_double(
             Gem::Specification,
             name: 'is_loaded',
-           
+            version: 'is_loaded_version',
+            gem_dir: '/is_loaded/'
+          )
+        ],
+      )
+
+      expect(code_provenance.generate).to have(1).item
+      expect(code_provenance.generate.first).to have_attributes(
+        name: 'is_loaded',
+        version: 'is_loaded_version',
+        path: '/is_loaded/',
+        kind: 'library',
+      )
+    end
+
+    it 'returns self' do
+      expect(code_provenance.refresh).to be code_provenance
+    end
+
+    context "when a gem's path is inside another gem's path" do
+      # I'm not entirely sure if this can happen in end-user apps, but can happen in CI if bundler is configured to
+      # install dependencies into a subfolder of ddtrace. In particular GitHub Actions does this.
+
+      it 'matches the loaded file to the longest matching path' do
+        code_provenance.refresh(
+          loaded_files: ['/dd-trace-rb/vendor/bundle/ruby/2.7.0/gems/byebug-11.1.3/lib/byebug.rb'],
+          loaded_specs: [
+            instance_double(
+              Gem::Specification,
+              name: 'ddtrace',
+              version: '1.2.3',
+              gem_dir: '/dd-trace-rb'
+            ),
+            instance_double(
+              Gem::Specification,
+              name: 'byebug',
+              version: '4.5.6',
+              gem_dir: '/dd-trace-rb/vendor/bundle/ruby/2.7.0/gems/byebug-11.1.3'
+            )
+          ],
+        )
+
+        expect(code_provenance.generate).to have(1).item
+        expect(code_provenance.generate.first).to have_attributes(name: 'byebug')
+      end
+    end
+  end
+
+  describe '#generate_json' do
+    before do
+      code_provenance.refresh
+    end
+
+    let(:code_provenance_schema) do
+      %(
+        {
+            "type": "object",
+            "required": [
+                "v1"
+            ],
+            "properties": {
+                "v1": {
+                    "type": "array",
+                    "additionalItems": true,
+                    "items": {
+                        "anyOf": [
+                            {
+                                "type": "object",
+                                "required": [
+                                    "kind",
+                                    "name",
+                                    "version",
+                                    "paths"
+                                ],
+                                "properties": {
+                                    "kind": {
+                                        "type": "string"
+                                    },
+                                    "name": {
+                                        "type": "string"
+                                    },
+                                    "version": {
+                                        "type": "string"
+                                    },
+                                    "paths": {
+                                        "type": "array",
+                                        "additionalItems": true,
+                                        "items": {
+                                            "anyOf": [
+                                                {
+                                                    "type": "string"
+                                                }
+                                            ]
+                                        }
+                                    }
+                                },
+                                "additionalProperties": true
+                            }
+                        ]
+                    }
+                }
+            },
+            "additionalProperties": true
+        }
+      ).freeze
+    end
+
+    it 'renders the list of loaded libraries as json' do
+      expect(JSON.parse(code_provenance.generate_json).fetch('v1')).to include(
+        hash_including(
+          'name' => 'stdlib',
+          'kind' => 'standard library',
+          'version' => RUBY_VERSION.to_s,
+          'paths' => include(start_with('/')),
+        ),
+        hash_including(
+          'name' => 'ddtrace',
+          'kind' => 'library',
+          'version' => DDTrace::VERSION::STRING,
+          'paths' => include(start_with('/')),
+        ),
+        hash_including(
+          'name' => 'rspec-core',
+          'kind' => 'library',
+          'version' => start_with('3.'), # This will one day need to be bumped for RSpec 4
+          'paths' => include(start_with('/')),
+        )
+      )
+    end
+
+    it 'renders the list of loaded libraries using the expected sch
