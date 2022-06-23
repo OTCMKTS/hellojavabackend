27b10663377c5bde4e7870348d6e19492eab22ee
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-06-23 10:12:21 +0000

    Moving files

--- spec/datadog/profiling/collectors/old_stack_spec.rb
+++ spec/datadog/profiling/collectors/old_stack_spec.rb
@@ -97,4 +97,150 @@ RSpec.describe Datadog::Profiling::Collectors::OldStack do
     end
   end
 
-  describe '#
+  describe '#perform' do
+    subject(:perform) { collector.perform }
+
+    after do
+      collector.stop(true, 0)
+      collector.join
+    end
+
+    context 'when disabled' do
+      before { collector.enabled = false }
+
+      it 'does not start a worker thread' do
+        perform
+
+        expect(collector.send(:worker)).to be nil
+
+        expect(collector).to have_attributes(
+          run_async?: false,
+          running?: false,
+          started?: false,
+          forked?: false,
+          fork_policy: :restart,
+          result: nil
+        )
+      end
+    end
+
+    context 'when enabled' do
+      before { collector.enabled = true }
+
+      after { collector.terminate }
+
+      it 'starts a worker thread' do
+        allow(collector).to receive(:collect_events)
+
+        perform
+
+        expect(collector.send(:worker)).to be_a_kind_of(Thread)
+        try_wait_until { collector.running? }
+
+        expect(collector).to have_attributes(
+          run_async?: true,
+          running?: true,
+          started?: true,
+          forked?: false,
+          fork_policy: :restart,
+          result: nil
+        )
+      end
+    end
+  end
+
+  describe '#collect_and_wait' do
+    subject(:collect_and_wait) { collector.collect_and_wait }
+
+    let(:collect_time) { 0.05 }
+    let(:updated_wait_time) { rand }
+
+    before do
+      expect(collector).to receive(:collect_events)
+      allow(collector).to receive(:compute_wait_time)
+        .with(collect_time)
+        .and_return(updated_wait_time)
+
+      allow(Datadog::Core::Utils::Time).to receive(:measure) do |&block|
+        block.call
+        collect_time
+      end
+    end
+
+    it 'changes its wait interval after collecting' do
+      expect(collector).to receive(:loop_wait_time=)
+        .with(updated_wait_time)
+
+      collect_and_wait
+    end
+  end
+
+  describe '#collect_events' do
+    let(:options) { { **super(), thread_api: thread_api, max_threads_sampled: max_threads_sampled } }
+    let(:thread_api) { class_double(Thread, current: Thread.current) }
+    let(:threads) { [Thread.current] }
+    let(:max_threads_sampled) { 3 }
+
+    subject(:collect_events) { collector.collect_events }
+
+    before do
+      allow(thread_api).to receive(:list).and_return(threads)
+      allow(recorder).to receive(:push)
+    end
+
+    it 'produces stack events' do
+      is_expected.to be_a_kind_of(Array)
+      is_expected.to include(kind_of(Datadog::Profiling::Events::StackSample))
+    end
+
+    describe 'max_threads_sampled behavior' do
+      context 'when number of threads to be sample is <= max_threads_sampled' do
+        let(:threads) { Array.new(max_threads_sampled) { |n| instance_double(Thread, "Thread #{n}", alive?: true) } }
+
+        it 'samples all threads' do
+          sampled_threads = []
+          expect(collector).to receive(:collect_thread_event).exactly(max_threads_sampled).times do |thread, *_|
+            sampled_threads << thread
+          end
+
+          result = collect_events
+
+          expect(result.size).to be max_threads_sampled
+          expect(sampled_threads).to eq threads
+        end
+      end
+
+      context 'when number of threads to be sample is > max_threads_sampled' do
+        let(:threads) { Array.new(max_threads_sampled + 1) { |n| instance_double(Thread, "Thread #{n}", alive?: true) } }
+
+        it 'samples exactly max_threads_sampled threads' do
+          sampled_threads = []
+          expect(collector).to receive(:collect_thread_event).exactly(max_threads_sampled).times do |thread, *_|
+            sampled_threads << thread
+          end
+
+          result = collect_events
+
+          expect(result.size).to be max_threads_sampled
+          expect(threads).to include(*sampled_threads)
+        end
+
+        it 'eventually samples all threads' do
+          sampled_threads = Set.new
+          allow(collector).to receive(:collect_thread_event) { |thread, *_| sampled_threads << thread }
+
+          begin
+            Timeout.timeout(1) { collector.collect_events while sampled_threads.size != threads.size }
+          rescue Timeout::Error
+            raise 'Failed to eventually sample all threads in time given'
+          end
+
+          expect(threads).to contain_exactly(*sampled_threads.to_a)
+        end
+      end
+    end
+
+    context 'when the thread' do
+      let(:thread) { instance_double(Thread, alive?: alive?) }
+      let(:threads) { [thread] }
+      let(:alive?) { tru
