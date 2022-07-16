c157be75b04fe3c81fafdbd6b2a18b95302d1cba
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-07-16 14:17:28 +0000

    Add build status to readme.

--- spec/datadog/tracing/contrib/action_cable/patcher_spec.rb
+++ spec/datadog/tracing/contrib/action_cable/patcher_spec.rb
@@ -34,4 +34,128 @@ RSpec.describe 'ActionCable patcher' do
   end
 
   around do |example|
-   
+    # Reset before and after each example; don't allow global state to linger.
+    Datadog.registry[:action_cable].reset_configuration!
+    example.run
+    Datadog.registry[:action_cable].reset_configuration!
+  end
+
+  context 'with server' do
+    let(:channel) { 'chat_room' }
+    let(:message) { 'Hello Internet!' }
+
+    let(:server) do
+      ActionCable::Server::Base.new.tap do |s|
+        s.config.cable = { adapter: 'inline' }.with_indifferent_access
+        s.config.logger = Logger.new($stdout)
+      end
+    end
+
+    context 'on broadcast' do
+      subject(:broadcast) { server.broadcast(channel, message) }
+
+      it 'traces broadcast event' do
+        broadcast
+
+        expect(span.service).to eq(tracer.default_service)
+        expect(span.name).to eq('action_cable.broadcast')
+        expect(span.span_type).to eq('web')
+        expect(span.resource).to eq('action_cable.broadcast')
+        expect(span.get_tag('action_cable.channel')).to eq(channel)
+        expect(span.get_tag('action_cable.broadcast.coder')).to eq('ActiveSupport::JSON')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT)).to eq('action_cable')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_OPERATION))
+          .to eq('broadcast')
+        expect(span).to_not have_error
+      end
+
+      it_behaves_like 'analytics for integration' do
+        before do
+          ActiveSupport::Notifications.instrument(
+            Datadog::Tracing::Contrib::ActionCable::Events::Broadcast::EVENT_NAME
+          )
+        end
+
+        let(:analytics_enabled_var) { Datadog::Tracing::Contrib::ActionCable::Ext::ENV_ANALYTICS_ENABLED }
+        let(:analytics_sample_rate_var) { Datadog::Tracing::Contrib::ActionCable::Ext::ENV_ANALYTICS_SAMPLE_RATE }
+      end
+
+      it_behaves_like 'measured span for integration', false do
+        before do
+          ActiveSupport::Notifications.instrument(
+            Datadog::Tracing::Contrib::ActionCable::Events::Broadcast::EVENT_NAME
+          )
+        end
+      end
+    end
+  end
+
+  context 'with channel' do
+    let(:channel_class) do
+      stub_const(
+        'ChatChannel',
+        Class.new(ActionCable::Channel::Base) do
+          def subscribed; end
+
+          def unsubscribed; end
+
+          def foo(_data); end
+        end
+      )
+    end
+
+    let(:channel_instance) { channel_class.new(connection, '{id: 1}', id: 1) }
+    let(:connection) { double('connection', logger: Logger.new($stdout), transmit: nil, identifiers: []) }
+
+    context 'on subscribe' do
+      include_context 'Rails test application'
+
+      subject(:subscribe) { channel_instance.subscribe_to_channel }
+
+      before { app }
+
+      it 'traces the subscribe hook' do
+        subscribe
+
+        expect(span.service).to eq(tracer.default_service)
+        expect(span.name).to eq('action_cable.subscribe')
+        expect(span.span_type).to eq('web')
+        expect(span.resource).to eq('ChatChannel#subscribe')
+        expect(span.get_tag('action_cable.channel_class')).to eq('ChatChannel')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT)).to eq('action_cable')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_OPERATION)).to eq('subscribe')
+        expect(span).to_not have_error
+      end
+    end
+
+    context 'on unsubscribe' do
+      include_context 'Rails test application'
+
+      subject(:unsubscribe) { channel_instance.unsubscribe_from_channel }
+
+      before { app }
+
+      it 'traces the unsubscribe hook' do
+        unsubscribe
+
+        expect(span.service).to eq(tracer.default_service)
+        expect(span.name).to eq('action_cable.unsubscribe')
+        expect(span.span_type).to eq('web')
+        expect(span.resource).to eq('ChatChannel#unsubscribe')
+        expect(span.get_tag('action_cable.channel_class')).to eq('ChatChannel')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT)).to eq('action_cable')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_OPERATION)).to eq('unsubscribe')
+        expect(span).to_not have_error
+      end
+    end
+
+    context 'on perform action' do
+      subject(:perform) { channel_instance.perform_action(data) }
+
+      let(:data) { { 'action' => 'foo', 'extra' => 'data' } }
+
+      it 'traces perform action event' do
+        perform
+
+        expect(span.service).to eq(tracer.default_service)
+        expect(span.name).to eq('a
