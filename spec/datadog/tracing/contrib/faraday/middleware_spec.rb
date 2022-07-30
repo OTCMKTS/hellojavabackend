647ca9a561a2a8135d8904e9242cba73f713e8d5
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-07-30 08:20:38 +0000

    1.5

--- spec/datadog/tracing/contrib/faraday/middleware_spec.rb
+++ spec/datadog/tracing/contrib/faraday/middleware_spec.rb
@@ -50,4 +50,109 @@ RSpec.describe 'Faraday middleware' do
 
       expect(span).to_not be nil
       expect(span.service).to eq(Datadog::Tracing::Contrib::Faraday::Ext::DEFAULT_PEER_SERVICE_NAME)
-      expect(span.name).to eq(Datadog::T
+      expect(span.name).to eq(Datadog::Tracing::Contrib::Faraday::Ext::SPAN_REQUEST)
+      expect(span.resource).to eq('GET')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::HTTP::TAG_METHOD)).to eq('GET')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::HTTP::TAG_STATUS_CODE)).to eq('200')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::HTTP::TAG_URL)).to eq('/success')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::NET::TAG_TARGET_HOST)).to eq('example.com')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::NET::TAG_TARGET_PORT)).to eq(80)
+      expect(span.span_type).to eq(Datadog::Tracing::Metadata::Ext::HTTP::TYPE_OUTBOUND)
+      expect(span).to_not have_error
+
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT)).to eq('faraday')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_OPERATION)).to eq('request')
+      expect(span.get_tag('span.kind')).to eq('client')
+    end
+
+    it_behaves_like 'a peer service span' do
+      let(:peer_hostname) { 'example.com' }
+    end
+
+    it 'executes without warnings' do
+      expect { response }.to_not output(/WARNING/).to_stderr
+    end
+
+    context 'with default Faraday connection' do
+      subject(:response) { client.get('http://example.com/success') }
+
+      let(:client) { ::Faraday } # Use the singleton client
+
+      before do
+        # We mock HTTP requests we we can't configure
+        # the test adapter for the default connection
+        WebMock.enable!
+        stub_request(:get, /example.com/).to_return(status: 200)
+      end
+
+      after { WebMock.disable! }
+
+      it_behaves_like 'environment service name', 'DD_TRACE_FARADAY_SERVICE_NAME'
+
+      it 'uses default configuration' do
+        expect(response.status).to eq(200)
+
+        expect(span.service).to eq(Datadog::Tracing::Contrib::Faraday::Ext::DEFAULT_PEER_SERVICE_NAME)
+        expect(span.name).to eq(Datadog::Tracing::Contrib::Faraday::Ext::SPAN_REQUEST)
+        expect(span.resource).to eq('GET')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::HTTP::TAG_METHOD)).to eq('GET')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::HTTP::TAG_STATUS_CODE)).to eq('200')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::HTTP::TAG_URL)).to eq('/success')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::NET::TAG_TARGET_HOST)).to eq('example.com')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::NET::TAG_TARGET_PORT)).to eq(80)
+        expect(span.span_type).to eq(Datadog::Tracing::Metadata::Ext::HTTP::TYPE_OUTBOUND)
+        expect(span).to_not have_error
+
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT)).to eq('faraday')
+        expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_OPERATION)).to eq('request')
+        expect(span.get_tag('span.kind')).to eq('client')
+      end
+
+      it 'executes without warnings' do
+        expect { response }.to_not output(/WARNING/).to_stderr
+      end
+
+      context 'with basic auth' do
+        subject(:response) { client.get('http://username:password@example.com/success') }
+
+        it 'does not collect auth info' do
+          expect(response.status).to eq(200)
+
+          expect(span.get_tag('http.url')).to eq('/success')
+        end
+
+        it 'executes without warnings' do
+          expect { response }.to_not output(/WARNING/).to_stderr
+        end
+      end
+    end
+  end
+
+  context 'when there is no interference' do
+    subject!(:response) { client.get('/success') }
+
+    it_behaves_like 'environment service name', 'DD_TRACE_FARADAY_SERVICE_NAME'
+
+    it do
+      expect(response).to be_a_kind_of(::Faraday::Response)
+      expect(response.body).to eq('OK')
+      expect(response.status).to eq(200)
+    end
+  end
+
+  context 'when there is successful request' do
+    subject!(:response) { client.get('/success') }
+
+    it_behaves_like 'environment service name', 'DD_TRACE_FARADAY_SERVICE_NAME'
+
+    it_behaves_like 'analytics for integration' do
+      let(:analytics_enabled_var) { Datadog::Tracing::Contrib::Faraday::Ext::ENV_ANALYTICS_ENABLED }
+      let(:analytics_sample_rate_var) { Datadog::Tracing::Contrib::Faraday::Ext::ENV_ANALYTICS_SAMPLE_RATE }
+    end
+
+    it_behaves_like 'measured span for integration', false
+
+    it do
+      expect(span).to_not be nil
+      expect(span.service).to eq(Datadog::Tracing::Contrib::Faraday::Ext::DEFAULT_PEER_SERVICE_NAME)
+      expect(span.name).to eq(Datadog::Tracing::Contrib::Faraday::Ext::SPA
