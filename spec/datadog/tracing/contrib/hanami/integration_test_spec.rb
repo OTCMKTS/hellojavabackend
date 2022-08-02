2d5cfd36baa5e52064b4fdc5c2ccd5313d38faaf
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-08-02 10:43:10 +0000

    Regenerate gemspec for version 0.0.7

--- spec/datadog/tracing/contrib/hanami/integration_test_spec.rb
+++ spec/datadog/tracing/contrib/hanami/integration_test_spec.rb
@@ -41,4 +41,55 @@ RSpec.describe 'Hanami instrumentation' do
 
       expect(spans).to have(4).items
 
-      expect(rack_s
+      expect(rack_span).to be_hanami_rack_span.with(resource: 'Dummy::Controllers::Books::Index')
+      expect(routing_span).to be_hanami_routing_span.with(parent: rack_span, resource: 'Dummy::Controllers::Books::Index')
+      expect(action_span).to be_hanami_action_span.with(parent: routing_span, resource: 'Dummy::Controllers::Books::Index')
+      expect(render_span).to be_hanami_render_span.with(parent: routing_span, resource: 'Dummy::Controllers::Books::Index')
+    end
+  end
+
+  context 'when given an endpoint not found' do
+    subject(:response) { get 'not_found' }
+
+    it 'creates 3 spans' do
+      expect(response.status).to eq(404)
+
+      expect(spans).to have(3).items
+
+      expect(rack_span).to be_hanami_rack_span.with(resource: 'GET 404', http_method: 'GET', http_status_code: 404)
+      expect(routing_span).to be_hanami_routing_span.with(parent: rack_span, resource: 'GET')
+      expect(render_span).to be_hanami_render_span.with(
+        parent: routing_span,
+        resource: 'Hanami::Routing::Default::NullAction'
+      )
+    end
+  end
+
+  context 'when given an server error endpoint' do
+    subject(:response) { get 'server_error' }
+
+    it 'creates 4 spans' do
+      expect(response.status).to eq(500)
+
+      expect(spans).to have(4).items
+
+      expect(rack_span).to be_hanami_rack_span.with(
+        resource: 'Dummy::Controllers::Books::ServerError',
+        http_status_code: 500,
+        have_error: true
+      )
+      expect(routing_span).to be_hanami_routing_span.with(
+        parent: rack_span,
+        resource: 'Dummy::Controllers::Books::ServerError'
+      )
+      expect(action_span).to be_hanami_action_span.with(
+        parent: routing_span,
+        resource: 'Dummy::Controllers::Books::ServerError'
+      )
+      expect(render_span).to be_hanami_render_span.with(
+        parent: routing_span,
+        resource: 'Dummy::Controllers::Books::ServerError'
+      )
+    end
+  end
+end
