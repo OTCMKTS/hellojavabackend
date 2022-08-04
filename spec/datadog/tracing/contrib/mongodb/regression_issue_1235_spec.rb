e3a27e62f7c416d493fd40e904f812d15c0544d8
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-08-04 11:03:25 +0000

    Documentations

--- spec/datadog/tracing/contrib/mongodb/regression_issue_1235_spec.rb
+++ spec/datadog/tracing/contrib/mongodb/regression_issue_1235_spec.rb
@@ -20,3 +20,29 @@ RSpec.describe 'Mongo crash regression #1235' do
   end
 
   subject do
+    # CRuby returns a successful status code even during a crash,
+    # which makes testing this bug much harder.
+    # We resort to capturing STDERR and inspecting for errors.
+
+    pid = fork do
+      # Suppress all errors during Ruby instruction execution.
+      # When the VM is shutting down, this StringIO
+      # will be destroy, allowing the crash output to be
+      # outputted to STDERR.
+      $stderr = StringIO.new
+
+      client[:foo].insert_one(bar: 'baz')
+
+      exit(true) # Forcing an immediate Ruby VM exit causes the crash
+    end
+
+    _, status = Process.waitpid2(pid)
+    status
+  end
+
+  it 'does not crash on exit' do
+    expect { subject }.to_not output(/^\[BUG\] /).to_stderr_from_any_process
+
+    expect(subject).to be_success
+  end
+end
