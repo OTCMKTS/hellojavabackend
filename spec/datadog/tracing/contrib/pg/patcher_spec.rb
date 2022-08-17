d7bf3d71c49fcf71e3d37fe6be03d7b4e1ae3e8d
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-08-17 07:07:14 +0000

    restructured folders

--- spec/datadog/tracing/contrib/pg/patcher_spec.rb
+++ spec/datadog/tracing/contrib/pg/patcher_spec.rb
@@ -854,3 +854,95 @@ RSpec.describe 'PG::Connection patcher' do
         context 'when a failed query is made' do
           let(:sql_statement) { 'SELECT INVALID' }
 
+          subject(:async_exec) do
+            conn.async_exec(sql_statement) do |_pg_result|
+              # Do something with PG::Result
+            end
+          end
+
+          it_behaves_like 'with sql comment propagation', span_op_name: 'pg.async.exec', error: PG::Error
+
+          it 'traces failed queries' do
+            expect { async_exec }.to raise_error(PG::Error)
+            expect(spans.count).to eq(1)
+            expect(span).to have_error
+            expect(span).to have_error_message(include('ERROR') & include('column "invalid" does not exist'))
+          end
+
+          it_behaves_like 'environment service name', 'DD_TRACE_PG_SERVICE_NAME', error: PG::Error do
+            let(:configuration_options) { {} }
+          end
+        end
+      end
+    end
+
+    describe '#async_exec_params' do
+      before do
+        skip('pg < 1.1.0 does not support #async_exec_params') if Gem::Version.new(PG::VERSION) < Gem::Version.new('1.1.0')
+      end
+
+      let(:sql_statement) { 'SELECT $1::int;' }
+
+      context 'when without given a block' do
+        subject(:async_exec_params) { conn.async_exec_params(sql_statement, [1]) }
+
+        context 'when the tracer is disabled' do
+          before { tracer.enabled = false }
+
+          it 'does not write spans' do
+            async_exec_params
+
+            expect(spans).to be_empty
+          end
+        end
+
+        context 'when the tracer is configured directly' do
+          let(:service_name) { 'pg-override' }
+
+          before { Datadog.configure_onto(conn, service_name: service_name) }
+
+          it_behaves_like 'with sql comment propagation', span_op_name: 'pg.async.exec.params'
+
+          it 'produces a trace with service override' do
+            async_exec_params
+
+            expect(spans.count).to eq(1)
+            expect(span.service).to eq(service_name)
+            expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_PEER_SERVICE)).to eq(service_name)
+          end
+        end
+
+        context 'when a successful query is made' do
+          it_behaves_like 'with sql comment propagation', span_op_name: 'pg.async.exec.params'
+
+          it 'produces a trace' do
+            async_exec_params
+
+            expect(spans.count).to eq(1)
+            expect(span.name).to eq(Datadog::Tracing::Contrib::Pg::Ext::SPAN_ASYNC_EXEC_PARAMS)
+            expect(span.resource).to eq(sql_statement)
+            expect(span.service).to eq('pg')
+            expect(span.type).to eq(Datadog::Tracing::Metadata::Ext::SQL::TYPE)
+            expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_KIND))
+              .to eq(Datadog::Tracing::Metadata::Ext::SpanKind::TAG_CLIENT)
+            expect(span.get_tag(Datadog::Tracing::Contrib::Pg::Ext::TAG_DB_NAME)).to eq(dbname)
+            expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT))
+              .to eq(Datadog::Tracing::Contrib::Pg::Ext::TAG_COMPONENT)
+            expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_OPERATION))
+              .to eq(Datadog::Tracing::Contrib::Pg::Ext::TAG_OPERATION_QUERY)
+            expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_PEER_SERVICE))
+              .to eq(Datadog::Tracing::Contrib::Pg::Ext::DEFAULT_PEER_SERVICE_NAME)
+            expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_PEER_HOSTNAME)).to eq(host)
+            expect(span.get_tag(Datadog::Tracing::Contrib::Ext::DB::TAG_INSTANCE)).to eq(dbname)
+            expect(span.get_tag(Datadog::Tracing::Contrib::Ext::DB::TAG_USER)).to eq(user)
+            expect(span.get_tag('db.system')).to eq('postgresql')
+            expect(span.get_tag(Datadog::Tracing::Metadata::Ext::NET::TAG_TARGET_HOST)).to eq(host)
+            expect(span.get_tag(Datadog::Tracing::Metadata::Ext::NET::TAG_TARGET_PORT)).to eq(port.to_i)
+            expect(span.get_tag(Datadog::Tracing::Metadata::Ext::NET::TAG_DESTINATION_NAME)).to eq(host)
+            expect(span.get_tag(Datadog::Tracing::Metadata::Ext::NET::TAG_DESTINATION_PORT)).to eq(port.to_i)
+            expect(span.get_tag(Datadog::Tracing::Contrib::Ext::DB::TAG_ROW_COUNT)).to eq(1)
+          end
+
+          it_behaves_like 'analytics for integration' do
+            before { async_exec_params }
+            let(:analytics_enabled_var) { Datadog::Tracing::Contrib::Pg::Ext::ENV_ANAL
