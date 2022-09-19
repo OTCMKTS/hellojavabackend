6dbc2c8909c1feca3ebe2468e9baa204e71dcb15
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-09-19 20:00:41 +0000

    update draft

--- spec/datadog/tracing/contrib/rails/rails_active_job_spec.rb
+++ spec/datadog/tracing/contrib/rails/rails_active_job_spec.rb
@@ -40,4 +40,105 @@ RSpec.describe 'ActiveJob' do
         end
       )
       ExampleJob.discard_on(JobDiscardError) if ExampleJob.respond_to?(:discard_on)
-      ExampleJob.retry_on(JobRetryError, attempts: 2, wait: 2) { nil } if ExampleJob.respond_to?(
+      ExampleJob.retry_on(JobRetryError, attempts: 2, wait: 2) { nil } if ExampleJob.respond_to?(:retry_on)
+
+      ExampleJob
+    end
+
+    before do
+      Datadog.configure do |c|
+        c.tracing.instrument :active_job
+      end
+
+      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with('USE_TAGGED_LOGGING').and_return(true)
+
+      # initialize the application
+      app
+
+      # Override inline adapter to execute scheduled jobs for testingrails_active_job_spec
+      if ActiveJob::QueueAdapters::InlineAdapter.respond_to?(:enqueue_at)
+        allow(ActiveJob::QueueAdapters::InlineAdapter)
+          .to receive(:enqueue_at) do |job, _timestamp, *job_args|
+            ActiveJob::QueueAdapters::InlineAdapter.enqueue(job, *job_args)
+          end
+      else
+        allow_any_instance_of(ActiveJob::QueueAdapters::InlineAdapter)
+          .to receive(:enqueue_at) do |adapter, job, _timestamp|
+            adapter.enqueue(job)
+          end
+      end
+    end
+
+    it 'instruments enqueue' do
+      job_class.set(queue: :mice, priority: -10).perform_later
+
+      span = spans.find { |s| s.name == 'active_job.enqueue' }
+      expect(span.name).to eq('active_job.enqueue')
+      expect(span.resource).to eq('ExampleJob')
+      expect(span.get_tag('active_job.adapter')).to eq('ActiveJob::QueueAdapters::InlineAdapter')
+      expect(span.get_tag('active_job.job.id')).to match(/[0-9a-f\-]{32}/)
+      expect(span.get_tag('active_job.job.queue')).to eq('mice')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT))
+        .to eq('active_job')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_OPERATION))
+        .to eq('enqueue')
+
+      if Datadog::Tracing::Contrib::ActiveJob::Integration.version >= Gem::Version.new('5.0')
+        expect(span.get_tag('active_job.job.priority')).to eq(-10)
+      end
+    end
+
+    it 'instruments enqueue_at under the "enqueue" span' do
+      scheduled_at = 1.minute.from_now
+      job_class.set(queue: :mice, priority: -10, wait_until: scheduled_at).perform_later
+
+      span = spans.find { |s| s.name == 'active_job.enqueue' }
+      expect(span.name).to eq('active_job.enqueue')
+      expect(span.resource).to eq('ExampleJob')
+      expect(span.get_tag('active_job.adapter')).to eq('ActiveJob::QueueAdapters::InlineAdapter')
+      expect(span.get_tag('active_job.job.id')).to match(/[0-9a-f\-]{32}/)
+      expect(span.get_tag('active_job.job.queue')).to eq('mice')
+      expect(span.get_tag('active_job.job.scheduled_at').to_time).to be_within(1).of(scheduled_at)
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT))
+        .to eq('active_job')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_OPERATION))
+        .to eq('enqueue_at')
+
+      if Datadog::Tracing::Contrib::ActiveJob::Integration.version >= Gem::Version.new('5.0')
+        expect(span.get_tag('active_job.job.priority')).to eq(-10)
+      end
+    end
+
+    it 'instruments perform' do
+      job_class.set(queue: :elephants, priority: -10).perform_later
+
+      span = spans.find { |s| s.name == 'active_job.perform' }
+      expect(span.name).to eq('active_job.perform')
+      expect(span.resource).to eq('ExampleJob')
+      expect(span.get_tag('active_job.adapter')).to eq('ActiveJob::QueueAdapters::InlineAdapter')
+      expect(span.get_tag('active_job.job.id')).to match(/[0-9a-f\-]{32}/)
+      expect(span.get_tag('active_job.job.queue')).to eq('elephants')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT))
+        .to eq('active_job')
+      expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_OPERATION))
+        .to eq('perform')
+
+      if Datadog::Tracing::Contrib::ActiveJob::Integration.version >= Gem::Version.new('5.0')
+        expect(span.get_tag('active_job.job.priority')).to eq(-10)
+      end
+    end
+
+    it 'instruments active_job.enqueue_retry and active_job.retry_stopped' do
+      unless Datadog::Tracing::Contrib::ActiveJob::Integration.version >= Gem::Version.new('6.0')
+        skip('ActiveSupport instrumentation for Retry introduced in Rails 6')
+      end
+
+      job_class.set(queue: :elephants, priority: -10).perform_later(test_retry: true)
+
+      enqueue_retry_span = spans.find { |s| s.name == 'active_job.enqueue_retry' }
+      expect(enqueue_retry_span.name).to eq('active_job.enqueue_retry')
+      expect(enqueue_retry_span.resource).to eq('ExampleJob')
+      expect(enqueue_retry_span.get_tag('active_job.adapter')).to eq('ActiveJob::QueueAdapters::InlineAdapter')
+      expect(enqueue_retry_span.get_tag('active_job.job.id')).to match(/[0-9a-f\-]{32}/)
+      expect(enqueue_retry_span.get
