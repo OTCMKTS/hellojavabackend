c15c760a70d92eeca2c3179f64288f7ae628e868
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-10-13 22:42:13 +0000

    removed a file

---
+++ spec/datadog/tracing/contrib/rails/support/middleware.rb
@@ -0,0 +1,13 @@
+RSpec.shared_context 'Rails middleware' do
+  let(:rails_middleware) { [] }
+
+  let(:debug_middleware) do
+    stub_const(
+      'DebugMiddleware',
+      Class.new do
+        def initialize(app)
+          @app = app
+        end
+
+        def call(env)
+          @app.call(
