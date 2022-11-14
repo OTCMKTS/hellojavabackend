6aa668af8b43dd7fa28f377feda54eb5f5e0de46
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2022-11-14 09:10:12 +0000

    ARM: imx6: spin the cpu until hardware takes it down

--- spec/datadog/tracing/contrib/sql_comment_propagation_examples.rb
+++ spec/datadog/tracing/contrib/sql_comment_propagation_examples.rb
@@ -34,4 +34,41 @@ end
 RSpec.shared_examples_for 'propagates with sql comment' do |mode:, span_op_name:, error: nil|
   it "propagates with mode: #{mode}" do
     expect(Datadog::Tracing::Contrib::Propagation::SqlComment::Mode)
-      .to r
+      .to receive(:new).with(mode).and_return(propagation_mode)
+
+    if error
+      expect { subject }.to raise_error(error)
+    else
+      subject
+    end
+  end
+
+  it 'decorates the span operation' do
+    expect(Datadog::Tracing::Contrib::Propagation::SqlComment).to receive(:annotate!).with(
+      a_span_operation_with(name: span_op_name),
+      propagation_mode
+    )
+    if error
+      expect { subject }.to raise_error(error)
+    else
+      subject
+    end
+  end
+
+  it 'prepends sql comment to the sql statement' do
+    allow(Datadog::Tracing::Contrib::Propagation::SqlComment).to receive(:prepend_comment).and_call_original
+
+    if error
+      expect { subject }.to raise_error(error)
+    else
+      subject
+    end
+
+    expect(Datadog::Tracing::Contrib::Propagation::SqlComment).to have_received(:prepend_comment).with(
+      sql_statement,
+      a_span_operation_with(service: service_name),
+      duck_type(:to_digest),
+      propagation_mode
+    )
+  end
+end
