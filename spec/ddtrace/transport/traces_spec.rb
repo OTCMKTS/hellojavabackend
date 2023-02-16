6a1b710cf8f2909cdff879d76c5a6c72013b3b41
OTCMKTS <116132662+OTCMKTS@users.noreply.github.com>
2023-02-16 07:57:02 +0000

    Minor README updates.

--- spec/ddtrace/transport/traces_spec.rb
+++ spec/ddtrace/transport/traces_spec.rb
@@ -129,4 +129,116 @@ RSpec.describe Datadog::Transport::Traces::Transport do
 
   shared_context 'APIs with fallbacks' do
     let(:current_api_id) { :v2 }
-    let
+    let(:apis) do
+      Datadog::Transport::HTTP::API::Map[
+        v2: api_v2,
+        v1: api_v1
+      ].with_fallbacks(v2: :v1)
+    end
+
+    let(:api_v1) { instance_double(Datadog::Transport::HTTP::API::Instance, 'v1', encoder: encoder_v1) }
+    let(:api_v2) { instance_double(Datadog::Transport::HTTP::API::Instance, 'v2', encoder: encoder_v2) }
+    let(:encoder_v1) { instance_double(Datadog::Core::Encoding::Encoder, content_type: 'text/plain') }
+    let(:encoder_v2) { instance_double(Datadog::Core::Encoding::Encoder, content_type: 'text/csv') }
+  end
+
+  describe '#initialize' do
+    include_context 'APIs with fallbacks'
+
+    it { expect(subject.stats).to be_a(Datadog::Transport::Statistics::Counts) }
+
+    it { is_expected.to have_attributes(apis: apis, current_api_id: current_api_id) }
+  end
+
+  describe '#send_traces' do
+    include_context 'APIs with fallbacks'
+    include_context 'health metrics'
+
+    subject(:send_traces) { transport.send_traces(traces) }
+
+    let(:traces) { [] }
+    let(:response) { Class.new { include Datadog::Transport::Response }.new }
+    let(:responses) { [response] }
+
+    let(:encoded_traces) { double }
+    let(:trace_count) { 1 }
+    let(:chunks) { [[encoded_traces, trace_count]] }
+    let(:lazy_chunks) { chunks.lazy }
+
+    let(:request) { instance_double(Datadog::Transport::Traces::Request) }
+    let(:client_v2) { instance_double(Datadog::Transport::HTTP::Client) }
+    let(:client_v1) { instance_double(Datadog::Transport::HTTP::Client) }
+
+    let(:chunker) { instance_double(Datadog::Transport::Traces::Chunker, max_size: 1) }
+
+    before do
+      allow(Datadog::Transport::Traces::Chunker).to receive(:new).with(encoder_v1).and_return(chunker)
+      allow(Datadog::Transport::Traces::Chunker).to receive(:new).with(encoder_v2).and_return(chunker)
+
+      allow(chunker).to receive(:encode_in_chunks).and_return(lazy_chunks)
+
+      allow(Datadog::Transport::HTTP::Client).to receive(:new).with(api_v1).and_return(client_v1)
+      allow(Datadog::Transport::HTTP::Client).to receive(:new).with(api_v2).and_return(client_v2)
+      allow(client_v1).to receive(:send_traces_payload).with(request).and_return(response)
+      allow(client_v2).to receive(:send_traces_payload).with(request).and_return(response)
+
+      allow(Datadog::Transport::Traces::Request).to receive(:new).and_return(request)
+    end
+
+    context 'which returns an OK response' do
+      before { allow(response).to receive(:ok?).and_return(true) }
+
+      it 'sends to only the current API once' do
+        is_expected.to eq(responses)
+        expect(client_v2).to have_received(:send_traces_payload).with(request).once
+
+        expect(health_metrics).to have_received(:transport_chunked).with(1)
+      end
+
+      context 'with a runtime that returns eagerly loaded chunks' do
+        before do
+          if !PlatformHelpers.jruby? || PlatformHelpers.engine_version >= Gem::Version.new('9.2.9.0')
+            skip 'This runtime correctly returns lazy enumerators on Enumerator::Lazy#slice_before calls'
+          end
+        end
+
+        let(:lazy_chunks) { chunks }
+
+        it 'successfully sends a single request' do
+          is_expected.to eq(responses)
+          expect(client_v2).to have_received(:send_traces_payload).with(request).once
+
+          expect(health_metrics).to have_received(:transport_chunked).with(1)
+        end
+      end
+
+      context 'with many chunks' do
+        let(:chunks) { [[], []] }
+        let(:responses) { [response, response] }
+
+        it do
+          is_expected.to eq(responses)
+          expect(health_metrics).to have_received(:transport_chunked).with(2)
+        end
+      end
+    end
+
+    context 'which returns a not found response' do
+      before do
+        allow(response).to receive(:not_found?).and_return(true)
+        allow(response).to receive(:client_error?).and_return(true)
+      end
+
+      it 'attempts each API once as it falls back after each failure' do
+        is_expected.to eq(responses)
+
+        expect(client_v2).to have_received(:send_traces_payload).with(request).once
+        expect(client_v1).to have_received(:send_traces_payload).with(request).once
+
+        expect(health_metrics).to have_received(:transport_chunked).with(1)
+      end
+    end
+
+    context 'which returns an unsupported response' do
+      before do
+        allow(response).to receive(:unsupported?).and_ret
